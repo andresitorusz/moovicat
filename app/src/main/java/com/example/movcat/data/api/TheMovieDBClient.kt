c83f33be2214747a8a4d036f89c15da2643d6f9d
Andre Yehuda Sitorus <36465150+andresitorusz@users.noreply.github.com>
2020-12-12 02:47:58 +0700

    Change the poster size to original

--- app/src/main/java/com/example/movcat/data/api/TheMovieDBClient.kt
+++ app/src/main/java/com/example/movcat/data/api/TheMovieDBClient.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit
 
 const val API_KEY = "6a2b8629fcc338600751630494627b3e"
 const val BASE_URL = "https://api.themoviedb.org/3/"
-const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342"
+const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/original"
 
 const val FIRST_PAGE = 1
 const val POST_PER_PAGE = 994
